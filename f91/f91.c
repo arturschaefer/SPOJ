4b56966a2f7cda2ed71d782f43d82dd1e3acc9fc
Artur Schaefer <arturschaefer@users.noreply.github.com>
2016-06-11 23:36:08 -0300

    Indentation changes

--- f91/f91.c
+++ f91/f91.c
@@ -16,8 +16,6 @@ int main(void){
 }
 
 int f91(int num){
-    if(num > 100)
-        return num - 10;
-    else
-        return f91( f91 (num + 11));
+    if(num > 100)   return num - 10;
+    else    return f91( f91 (num + 11));
 }
