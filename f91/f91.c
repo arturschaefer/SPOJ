a59ae66d833e21af761f4d382b5bd0bd88fa41ea
Artur Schaefer <arturschaefer@users.noreply.github.com>
2016-06-11 23:37:47 -0300

    Fixing code

--- f91/f91.c
+++ f91/f91.c
@@ -8,14 +8,13 @@ int main(void){
     do{
         scanf("%d", &N);
         if(N == 0)  break;
-        if (!(N + 10 > 100) || N > 100)
-            num_f91 = f91(N);
+        num_f91 = f91(N);
         printf("f91(%d) = %d\n", N, num_f91);
     }while ( N != 0);
     return 0;
 }
 
 int f91(int num){
-    if(num > 100)   return num - 10;
-    else    return f91( f91 (num + 11));
+    if(num > 100) return num - 10;
+    else return f91( f91 (num + 11));
 }
